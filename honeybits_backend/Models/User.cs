3016c3ab1a86e7581c204c888493708b6b35172e
Maximo Lopez <mlopez@cdevteam.com>
2019-06-30 14:42:08 -0400

    Initial Commit

---
+++ honeybits_backend/Models/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace honeybits_backend.Models {
+    public class User {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        
+    }
+}
