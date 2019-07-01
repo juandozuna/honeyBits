6de3fed862cd656abb7ef8b506bb1f4bb69d5650
Maximo Lopez <mlopez@cdevteam.com>
2019-07-01 11:21:59 -0400

    added Username, Password and Role fields to User model

--- honeybits_backend/Models/User.cs
+++ honeybits_backend/Models/User.cs
@@ -1,4 +1,5 @@
 using System;
+using honeybits_backend.Models.Enums;
 
 namespace honeybits_backend.Models {
     public class User {
@@ -6,6 +7,10 @@ namespace honeybits_backend.Models {
         public String FirstName { get; set; }
         public String LastName { get; set; }
         public DateTime Birthdate { get; set; }
+
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public Role Role { get; set; }
         
     }
 }
