76cf31607274ec16c7ae7b6acaaa3344576359ca
Juan Daniel Ozuna Espinal <juandanielozuna2@gmail.com>
2019-07-01 07:11:43 -0400

    iOS project initial commit

---
+++ honeybits_ios/HoneyBitsTests/HoneyBitsTests.swift
@@ -0,0 +1,34 @@
+//
+//  HoneyBitsTests.swift
+//  HoneyBitsTests
+//
+//  Created by Juan Daniel Ozuna Espinal on 7/1/19.
+//  Copyright © 2019 Juan Daniel Ozuna Espinal. All rights reserved.
+//
+
+import XCTest
+@testable import HoneyBits
+
+class HoneyBitsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
