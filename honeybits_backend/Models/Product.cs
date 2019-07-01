6706ddb49ebd13a829f493b62b188e7616d802c2
Maximo Lopez <mlopez@cdevteam.com>
2019-07-01 09:16:22 -0400

    Product model added

---
+++ honeybits_backend/Models/Product.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace honeybits_backend.Models {
+    public class Product {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public decimal Price { get; set; }
+        
+    }
+}
