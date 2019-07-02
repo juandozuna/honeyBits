83054cb6fa9a914254a613bc3575faa39a875179
Maximo Lopez <mlopez@cdevteam.com>
2019-07-01 20:50:59 -0400

    Added static URL for debugging

--- honeybits_backend/Program.cs
+++ honeybits_backend/Program.cs
@@ -19,6 +19,7 @@ namespace honeybits_backend
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000");
     }
 }
