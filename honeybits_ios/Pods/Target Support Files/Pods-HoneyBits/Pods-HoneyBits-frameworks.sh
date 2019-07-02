bb08e9779e607ee01ecfac26efb3f50e83284066
Juan Daniel Ozuna Espinal <juandanielozuna2@gmail.com>
2019-07-01 23:02:38 -0400

    Started building Login UI Popup

--- honeybits_ios/Pods/Target Support Files/Pods-HoneyBits/Pods-HoneyBits-frameworks.sh
+++ honeybits_ios/Pods/Target Support Files/Pods-HoneyBits/Pods-HoneyBits-frameworks.sh
@@ -163,7 +163,6 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ChameleonFramework/ChameleonFramework.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/LoadingButton/LoadingButton.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PMSuperButton/PMSuperButton.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwipeCellKit/SwipeCellKit.framework"
@@ -171,7 +170,6 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ChameleonFramework/ChameleonFramework.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/LoadingButton/LoadingButton.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PMSuperButton/PMSuperButton.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwipeCellKit/SwipeCellKit.framework"
