dbb8ccb7693ac090f2c84c27863f0bf1ac4acd92
Maximo Lopez <mlopez@cdevteam.com>
2019-07-10 15:59:36 -0400

    New implementation to Services from Interfaces update

--- honeybits_server/honeybits_server/Services/ProductService.cs
+++ honeybits_server/honeybits_server/Services/ProductService.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using honeybits_server.Models;
+using honeybits_server.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace honeybits_server.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        honeybitsContext _context = new honeybitsContext();
+
+        public Product Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id) => _context.Product.Find(id);
+
+        public IEnumerable<Product> GetAll() => _context.Product.ToList();
     }
 }
--- honeybits_server/honeybits_server/Services/UserService.cs
+++ honeybits_server/honeybits_server/Services/UserService.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using honeybits_server.Models;
+using honeybits_server.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace honeybits_server.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public Token Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Users> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
