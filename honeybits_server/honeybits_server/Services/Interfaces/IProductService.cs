4b03cd22eea78cce9d647d02e15e4a56e5c87426
Maximo Lopez <mlopez@cdevteam.com>
2019-07-10 15:58:55 -0400

    Methods added to Services Interfaces

--- honeybits_server/honeybits_server/Services/Interfaces/IProductService.cs
+++ honeybits_server/honeybits_server/Services/Interfaces/IProductService.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using honeybits_server.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace honeybits_server.Services.Interfaces
 {
-    public class IProductService
+    public interface IProductService
     {
+        Product Get(int id);
+        Product Create(Product product);
+        void Delete(int id);
+        IEnumerable<Product> GetAll();
     }
 }
--- honeybits_server/honeybits_server/Services/Interfaces/IUserService.cs
+++ honeybits_server/honeybits_server/Services/Interfaces/IUserService.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using honeybits_server.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace honeybits_server.Services.Interfaces
 {
-    public class IUserService
+    public interface IUserService
     {
+        Token Authenticate(string username, string password);
+        IEnumerable<Users> GetAll();
     }
 }
