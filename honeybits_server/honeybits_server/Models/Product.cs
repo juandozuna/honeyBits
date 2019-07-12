72649667b2cb1f0ed971dd2a5265b3f0b41ae466
Maximo Lopez <mlopez@cdevteam.com>
2019-07-12 16:32:46 -0400

    Update on Models validators

--- honeybits_server/honeybits_server/Models/Product.cs
+++ honeybits_server/honeybits_server/Models/Product.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace honeybits_server.Models
 {
     public partial class Product
     {
+        [Key]
         public uint ProductId { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Minimum lenght is 2")]
+        [MaxLength(250, ErrorMessage = "Max lenght is 250")]
         public string ProductName { get; set; }
+        [Required]
         public int ProductCategoryId { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Minimum lenght is 2")]
+        [MaxLength(250, ErrorMessage = "Max lenght is 250")]
         public string ProductDescription { get; set; }
+        [Required]
         public decimal? ProductPrice { get; set; }
         public int CreatedBy { get; set; }
-        public DateTime? CreatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
     }
 }
--- honeybits_server/honeybits_server/Models/Users.cs
+++ honeybits_server/honeybits_server/Models/Users.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace honeybits_server.Models
 {
     public partial class Users
     {
+        [Key]
         public uint UserId { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Minimum lenght is 2")]
+        [MaxLength(250, ErrorMessage = "Max lenght is 250")]
         public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Minimum lenght is 2")]
+        [MaxLength(250, ErrorMessage = "Max lenght is 250")]
         public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
+
+        [Required]
         public DateTime Birtdate { get; set; }
+        [Required]
         public int RoleId { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Passwd { get; set; }
-        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
     }
 }
