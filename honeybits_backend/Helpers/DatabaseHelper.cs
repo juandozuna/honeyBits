cceacfb598d2a28db9d2f54512c040455456e658
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2019-07-16 11:17:46 -0400

    honeybits_backend project deleted

--- honeybits_backend/Helpers/DatabaseHelper.cs
+++
@@ -1,7 +0,0 @@
-namespace honeybits_backend.Helpers
-{
-    public class DatabaseHelper
-    {
-        public string ConnectionString { get; set; }
-    }
-}
--- honeybits_backend/Models/Token.cs
+++
@@ -1,7 +0,0 @@
-namespace honeybits_backend.Models {
-    public class Token {
-        public string token { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
