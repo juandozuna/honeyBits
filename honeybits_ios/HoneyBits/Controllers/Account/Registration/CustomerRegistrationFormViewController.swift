934f18dca51917eef1eb14fb61b977692b07dfa2
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-07-21 22:31:35 -0400

    Finished building base structure for registration screen

---
+++ honeybits_ios/HoneyBits/Controllers/Account/Registration/CustomerRegistrationFormViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CustomerRegistrationFormViewController.swift
+//  HoneyBits
+//
+//  Created by Juan Daniel Ozuna Espinal on 7/21/19.
+//  Copyright © 2019 Juan Daniel Ozuna Espinal. All rights reserved.
+//
+
+import UIKit
+
+class CustomerRegistrationFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
