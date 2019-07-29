9d58ebd6c929a686b9a3e1a20ae162c908e59099
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-07-28 22:11:02 -0400

    Update to top tab bars

---
+++ honeybits_ios/HoneyBits/Controllers/Generic/TopTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  TopTabBarController.swift
+//  HoneyBits
+//
+//  Created by Juan Daniel Ozuna Espinal on 7/28/19.
+//  Copyright © 2019 Juan Daniel Ozuna Espinal. All rights reserved.
+//
+
+import Foundation
