1afc7dda14a33aa13d8b247da68ddab2fd3eccad
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-07-28 22:11:22 -0400

    Made the top tab bar class a bit generic, prepping for animation

--- honeybits_ios/HoneyBits/Controllers/Generic/TopTabBarController.swift
+++ honeybits_ios/HoneyBits/Controllers/Generic/TopTabBarController.swift
@@ -6,4 +6,69 @@
 //  Copyright © 2019 Juan Daniel Ozuna Espinal. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ChameleonFramework
+
+class TopTabBarController : UITabBarController {
+    
+    @IBInspectable var tabColor: UIColor {
+        set {
+            tabBar.backgroundColor = newValue
+        }
+        get {
+            return tabBar.backgroundColor ?? UIColor.clear
+        }
+    }
+    
+    
+    @IBInspectable var tabItemColor: UIColor {
+        get {
+            return tabBar.tintColor
+        }
+        set {
+            tabBar.tintColor = newValue
+        }
+    }
+    
+    @IBInspectable var tabBarHeight: CGFloat = 38
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        runSetupMethods()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        runSetupMethods()
+    }
+    
+    private func runSetupMethods() {
+        tabBarPositioning()
+    }
+    
+    private func tabBarPositioning() {
+        let topPosition: CGFloat = -3
+        tabBar.frame = CGRect(x: 0, y: topPosition, width: UIScreen.main.bounds.width, height: tabBarHeight)
+        tabColor = UIColor.white
+        tabItemColor = UIColor.flatOrange()
+    }
+    
+    private func setContraintsToAllViewControllers() {
+        for vc in viewControllers! {
+            updateContstraint(to: vc)
+        }
+    }
+    
+    private func updateContstraint(to viewController: UIViewController) {
+        let controllerView = viewController.view!
+        let navHeightMultiplier = navigationController?.navigationBar.bounds.height ?? 0
+        let amountToAddToHeight = (tabBarHeight + navHeightMultiplier)
+        controllerView.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: amountToAddToHeight + 12)
+        controllerView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height - amountToAddToHeight - 10)
+    }
+    
+
+    
+}
--- honeybits_ios/HoneyBits/Controllers/Keeper/Dashboard/KeeperDashboardNavigationController.swift
+++ honeybits_ios/HoneyBits/Controllers/Keeper/Dashboard/KeeperDashboardNavigationController.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2019 Juan Daniel Ozuna Espinal. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class KeeperDashboardNavigationController : UINavigationController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        controllerSetup()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        controllerSetup()
+    }
+    
+    private func controllerSetup() {
+        
+       setRootTabbarController()
+    }
+    
+    private func setRootTabbarController() {
+        let tabBarController = loadTabBarController()
+        
+        //setViewControllers([tabBarController], animated: false)
+    }
+    
+    private func loadTabBarController() -> UITabBarController {
+        guard let tbc = viewControllerFromStoryboard(storyboard: "KeeperDashboard", withIdentifier: "tabBarController") as? TopTabBarController else {
+            print ("Error trying to load controller from storyboard")
+            return UITabBarController()
+        }
+        
+        return tbc
+    }
+}
+
