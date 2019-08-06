359328a55e77469ab7e36cebc3021afe159ff7ab
Maximo Lopez <mlopez@cdevteam.com>
2019-08-05 20:30:29 -0400

    Swagger configuration added

--- honeybits_server/honeybits_server/Startup.cs
+++ honeybits_server/honeybits_server/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace honeybits_server
 {
@@ -33,6 +34,19 @@ namespace honeybits_server
         {
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "1",
+                    Title = "Honeybits API",
+                    Description = "Honeybits API methods and functions.",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "Maximo Lopez", Email = "maximo.lopez.dev@gmail.com", Url = "None"}
+
+                });
+            });
 
             var appSettingsSection = Configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingsSection);
@@ -83,6 +97,12 @@ namespace honeybits_server
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
