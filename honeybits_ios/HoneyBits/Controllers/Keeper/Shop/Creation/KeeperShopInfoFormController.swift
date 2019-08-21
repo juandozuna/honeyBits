6f1f2ee383c6589af74b4123c0ed1e679c87cf52
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-08-20 23:28:22 -0400

    Built view to capture shop name

--- honeybits_ios/HoneyBits/Controllers/Keeper/Shop/Creation/KeeperShopInfoFormController.swift
+++ honeybits_ios/HoneyBits/Controllers/Keeper/Shop/Creation/KeeperShopInfoFormController.swift
@@ -8,24 +8,51 @@
 
 import UIKit
 import Material
+import SwiftValidators
+import PMSuperButton
 
 class KeeperShopInfoFormController: UIViewController {
 
     @IBOutlet weak var shopNameTxt: TextField!
+    @IBOutlet var bgView: UIView!
+    @IBOutlet weak var continueBtn: PMSuperButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         controllerSetup()
+        
+        bgView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.handleResigneResponderTap(_:))))
     }
     
     
     private func controllerSetup() {
+        continueBtn.isEnabled = false
         configureTextfields()
     }
     
-    
     private func configureTextfields() {
+        shopNameTxt.delegate = self
         setTextFieldColor(to: shopNameTxt)
     }
+    
+    private func resignResponderForFields() {
+        resignResponder(for: shopNameTxt)
+    }
+    
+    private func updateBtnStatus() {
+        let res = Validator.required().apply(shopNameTxt.text!)
+            continueBtn.isEnabled = res
+    }
+    
+    
+    @objc func handleResigneResponderTap(_ sender: UITapGestureRecognizer) {
+        resignResponderForFields()
+    }
+    
+}
 
+extension KeeperShopInfoFormController : UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateBtnStatus()
+    }
 }
