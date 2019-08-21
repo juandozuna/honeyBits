b8e346bf75c7dc5d18ff49131c06b6ef4e6cb531
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-08-20 23:33:55 -0400

    Added new controller file

---
+++ honeybits_ios/HoneyBits/Controllers/Keeper/Shop/Creation/KeeperShopLogoFormController.swift
@@ -0,0 +1,9 @@
+//
+//  KeeperShopLogoFormController.swift
+//  HoneyBits
+//
+//  Created by Juan Daniel Ozuna Espinal on 8/20/19.
+//  Copyright © 2019 Juan Daniel Ozuna Espinal. All rights reserved.
+//
+
+import Foundation
