19d2fde2534175683230f93bb50676dfae667234
Maximo Lopez <mlopez@cdevteam.com>
2019-09-20 14:58:59 -0400

    ProductImageDTO added

---
+++ honeybits_server/honeybits_server/DTOs/ProductImageDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace honeybits_server.DTOs
+{
+    public class ProductImageDTO
+    {
+        public int ProductImageId { get; set; }
+        public string ProductImageName { get; set; }
+        public string ProductImageDesc { get; set; }
+        public string ProductImageType { get; set; }
+        public string ImageContent { get; set; }
+    }
+}
