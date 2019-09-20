0a1983633c3c6af984075cc54ef133b91e473276
Maximo Lopez <mlopez@cdevteam.com>
2019-09-20 14:59:11 -0400

    ProductDTO added

---
+++ honeybits_server/honeybits_server/DTOs/ProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using honeybits_server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace honeybits_server.DTOs
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductCategoryId { get; set; }
+
+        public IEnumerable<ProductImageDTO> ProductImages { get; set; }
+    }
+}
