1eaf8fec98c33bd203e594750f93553aa638ebfc
Maximo Lopez <mlopez@cdevteam.com>
2019-10-05 04:39:41 -0400

    Update on IProductImageService, implemented DTO

--- honeybits_server/honeybits_server/Services/Interfaces/IProductImageService.cs
+++ honeybits_server/honeybits_server/Services/Interfaces/IProductImageService.cs
@@ -5,7 +5,7 @@ namespace honeybits_server.Services.Interfaces {
     public interface IProductImageService
     {
         ProductImageDTO Get(int Id);
-        ProductImageDTO Create(ProductImageDTO data);
+        IEnumerable<ProductImageDTO> Create(List<ProductImageDTO> data);
         bool Delete(int Id);
         IEnumerable<ProductImageDTO> GetAllProductsImages(int productId);
         IEnumerable<ProductImageDTO> GetAllImages();
