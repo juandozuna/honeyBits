e5185009c5aabcfb35c53216e83ea2be22262d45
Maximo Lopez <mlopez@cdevteam.com>
2019-10-05 04:40:49 -0400

    Added dependecy injection to ProductImageService

--- honeybits_server/honeybits_server/Services/ProductImageService.cs
+++ honeybits_server/honeybits_server/Services/ProductImageService.cs
@@ -1,12 +1,42 @@
+using System;
 using System.Collections.Generic;
 using honeybits_server.DTOs;
+using honeybits_server.Helpers;
+using honeybits_server.Models;
 using honeybits_server.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace honeybits_server.Services {
     public class ProductImageService : IProductImageService
     {
-        public ProductImageDTO Create(ProductImageDTO data)
+        honeybitsContext _context = new honeybitsContext();
+        private readonly IHostingEnvironment _hostingEnviroment;
+        private readonly AppSettings _appSettings;
+
+        public ProductImageService(IHostingEnvironment hostingEnvironment, IOptions<AppSettings> appSettings)
         {
+            _hostingEnviroment = hostingEnvironment;
+            _appSettings = appSettings.Value;
+        }
+
+        public IEnumerable<ProductImageDTO> Create(List<ProductImageDTO> data)
+        {
+            List<ProductImage> images = new List<ProductImage>();
+
+            foreach(var image in data) 
+            {
+                images.Add(new ProductImage {
+                    ProductId = image.ProductId,
+                    ProductImageName = image.ProductImageName,
+                    ProductImageDescription = image.ProductImageDesc,
+                    ProductImageType = image.ProductImageType,
+                    
+                    CreatedDate = DateTime.Now,
+
+                });
+            }
+
             throw new System.NotImplementedException();
         }
 
