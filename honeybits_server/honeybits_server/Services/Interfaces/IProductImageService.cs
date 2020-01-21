3c6f14543776d077281752268bad21eb78599a28
Maximo Lopez <maximo.lopez.dev@gmail.com>
2020-01-21 03:53:46 -0400

    GetByUrl method added to Interface

--- honeybits_server/honeybits_server/Services/Interfaces/IProductImageService.cs
+++ honeybits_server/honeybits_server/Services/Interfaces/IProductImageService.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.IO;
 using honeybits_server.DTOs;
 
 namespace honeybits_server.Services.Interfaces {
     public interface IProductImageService
     {
         ProductImageDTO Get(int Id);
+        ProductImageDTO ImageByUrl(string imageUrl);
         IEnumerable<ProductImageDTO> Create(List<ProductImageDTO> data);
         bool Delete(int Id);
         IEnumerable<ProductImageDTO> GetAllProductsImages(int productId);
